87c6c61175a0a658412feac886a7b79f9eb1adcf
Ashwin Kannan <classifieddatabank@Ashwins-MacBook-Pro.local>
2019-08-29 15:00:13 -0500

    lab complete

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,29 @@
 class Waiter
+  @@all = []
 
+  def initialize(name, exp)
+    @name = name
+    @exp = exp
+    @@all << self
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.waiter == self}
+  end
+
+  def best_tipper
+    Meal.all.inject {|memo, meal| memo.tip < meal.tip ? meal : memo}.customer
+  end
+
+  def customers
+    meals.map {|meal| meal.customer}
+  end
+
+  def self.all
+    @@all
+  end
 end
