72b91dad6f4f1a1a5b4cec4a0ba19110ca2f9736
github username <github email address>
2019-08-29 15:09:02 -0500

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,31 @@
 class Waiter
 
+    attr_reader :name, :exp
+    @@all = []
+
+    def initialize(name, exp)
+        @name = name
+        @exp = exp
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end 
+
+    def new_meal(customer, total, tip)
+        meal = Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        tips = meals.map {|meal| [meal.tip,meal.customer.name]}
+        tips = tips.sort
+        best_customer = tips.last[-1]
+        Customer.all.find {|customer| customer.name == best_customer}
+    end
+
 end
